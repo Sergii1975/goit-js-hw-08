c166118c776c5ee3fd8efbb680973f89dc2c82e4
Sergii <svbulgakov75@gmail.com>
2023-05-10 08:43:13 +0300

    10.05.2023

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const list = document.querySelector('ul.gallery');
 
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join('');
+
+list.insertAdjacentHTML('afterbegin', markup);
 console.log(galleryItems);
