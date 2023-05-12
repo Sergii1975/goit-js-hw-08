e240fff484133eb429db98d0ceb8a44bf77cd154
Sergii <svbulgakov75@gmail.com>
2023-05-12 23:18:20 +0300

    number2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const PLAYTIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(function(currentTime) {
+        const seconds = currentTime.seconds;
+        localStorage.setItem(PLAYTIME_KEY, JSON.stringify(seconds));
+    }, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(PLAYTIME_KEY)) || null);
 
