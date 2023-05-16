bbac9be1dd9e7a6e2a6208c4af9156652496f378
Sergii <svbulgakov75@gmail.com>
2023-05-16 13:39:59 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,29 +6,30 @@ const textareaEl = document.querySelector('textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const dataEl = {
+let dataEl = {
     email: '',
     message: '',
-};
+} || {};
 
 form.addEventListener('input', throttle(onFormTextInput, 500));
 fillTextarea();
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+  console.log(dataEl);
   if (Object.values(dataEl).some(value => value === ''.trim())) {
     alert('Fill all fields, please!')
   }
     localStorage.removeItem(STORAGE_KEY);
   event.currentTarget.reset();
-  console.log(dataEl);
+  dataEl = {};
 });
 
 function onFormTextInput(event) {
   dataEl[event.target.name] = event.target.value;
   const changedData = JSON.stringify(dataEl);
   localStorage.setItem(STORAGE_KEY, changedData);
- 
+  
 };
 function fillTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
